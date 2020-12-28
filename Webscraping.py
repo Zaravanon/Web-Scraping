59d3fc786e97b2243383ffffd380ae4e8ccae4a9
Saravanan <65016991+Zaravanon@users.noreply.github.com>
2020-12-28 20:03:01 +0530

    Add files via upload

---
+++ Webscraping.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+page=requests.get("https://en.wikipedia.org/wiki/List_of_India_ODI_cricketers").text
+#page
+soup=BeautifulSoup(page,"lxml")
+print(soup.prettify())
+table=soup.find("table",{"class":"wikitable"})
+data_rows = table.findAll('tr')[2:]
+#type(data_rows)
+player_names=[]
+for i in data_rows:
+    names=i.findAll("a")
+    for x in names:
+        player_names.append(x.get("title"))
+print(player_names)
+dataframe=pd.DataFrame()
+dataframe['player_names']= Players
+#dataframe.head()
+dataframe.to_csv("Cricket_players.csv")
